58ff5a0c703171d08f1df8a03d17eb3c040c9cea
PODURI SESHA SAI SATHWIK <158552902+Sai-Sathwik2718@users.noreply.github.com>
2026-05-25 11:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (86.41%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] t=new int[n+1];
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            int steal=nums[i-1]+t[i-2];
+            int skip=t[i-1];
+            t[i]=Math.max(steal,skip);
+        }
+        return t[n];
+
+
+    }
+}
